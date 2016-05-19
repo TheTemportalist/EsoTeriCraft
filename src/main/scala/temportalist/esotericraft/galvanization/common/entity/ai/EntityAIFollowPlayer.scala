12fe58d58a0b8dc0a77e64f60589027a68a2fbc4
TheTemportalist <the.country.gamer@gmail.com>
2016-05-19 13:56:56 -0400

    Patch AI to follow player with flying entities

--- src/main/scala/temportalist/esotericraft/galvanization/common/entity/ai/EntityAIFollowPlayer.scala
+++ src/main/scala/temportalist/esotericraft/galvanization/common/entity/ai/EntityAIFollowPlayer.scala
@@ -2,7 +2,6 @@ package temportalist.esotericraft.galvanization.common.entity.ai
 
 import net.minecraft.entity.ai.EntityAIBase
 import net.minecraft.entity.{EntityCreature, EntityLivingBase}
-import net.minecraft.util.math.MathHelper
 import temportalist.origin.api.common.lib.Vect
 
 /**
@@ -24,93 +23,41 @@ class EntityAIFollowPlayer(
 	override def shouldExecute(): Boolean = {
 		this.followingEntity = this.owner.getEntityWorld.getClosestPlayerToEntity(this.owner, this.radius)
 
-		if (this.canFly) {
-			val moveHelper = this.owner.getMoveHelper
-			/*
-			if (moveHelper.isUpdating) {
-				val diffX = moveHelper.getX - this.owner.posX
-				val diffY = moveHelper.getY - this.owner.posY
-				val diffZ = moveHelper.getZ - this.owner.posZ
-				val dist = diffX * diffX + diffY * diffY + diffZ + diffZ
-				return 3600 < dist || dist < 1D
-			}
-			//else return true
-			*/
-		}
-
 		this.followingEntity != null
 	}
 
 	override def startExecuting(): Unit = {
 		if (this.followingEntity != null)
 			this.followingPos = new Vect(this.followingEntity)
-
-		if (this.canFly) {
-			if (this.followingPos != null) {
-				//Galvanize.log(this.followingPos.toString)
-				//if (!this.owner.getMoveHelper.isUpdating)
-				///*
-				this.owner.getMoveHelper.setMoveTo(
-					this.followingPos.x,
-					this.followingPos.y,
-					this.followingPos.z,
-					this.speed
-				)
-				//*/
-			}
-		}
 	}
 
 	override def continueExecuting(): Boolean = super.continueExecuting()//!this.canFly
 
 	override def updateTask(): Unit = {
-		//if (this.canFly) return
 
 		this.owner.getLookHelper.setLookPositionWithEntity(
 			this.followingEntity, this.owner.getHorizontalFaceSpeed + 20,
 			this.owner.getVerticalFaceSpeed
 		)
 
-		if (canFly) {
-			val diffX = this.owner.posX - this.followingEntity.posX
-			val diffY = this.owner.posY - this.followingEntity.posY
-			val diffZ = this.owner.posZ - this.followingEntity.posZ
-			var dist = diffX * diffX + diffY * diffY + diffZ + diffZ
-			dist = MathHelper.sqrt_double(dist)
-			val xDiv = diffX / dist
-			val yDiv = diffY / dist
-			val zDiv = diffZ / dist
-
-			this.owner.motionX -= xDiv * 0.1D * this.speed
-			this.owner.motionY -= yDiv * 0.1D * this.speed
-			this.owner.motionZ -= zDiv * 0.1D * this.speed
-			//this.owner.moveEntityWithHeading(0, 0)
-
-			val aabb = this.owner.getEntityBoundingBox.offset(xDiv, yDiv, zDiv)
-			if (this.owner.worldObj.getCollisionBoxes(this.owner, aabb).isEmpty) {
-				//this.owner.getNavigator.clearPathEntity()
-				// is not colliding
-				//this.owner.motionX -= xDiv * 0.1D * this.speed
-				//this.owner.motionY -= yDiv * 0.1D * this.speed
-				//this.owner.motionZ -= zDiv * 0.1D * this.speed
-				//this.owner.motionY = -yDiv * 0.1D * this.speed
-				//this.owner.moveEntity(0, -yDiv, 0)
-				//this.owner.moveEntityWithHeading(0, 0)
+		if (this.owner.getDistanceSqToEntity(this.followingEntity) < 6.25D) { // proximity to player
+			if (!this.canFly) {
+				this.owner.getNavigator.clearPathEntity()
 			}
-			else {
-				//this.owner.getNavigator.tryMoveToEntityLiving(this.followingEntity, this.speed)
-			}
-
 		}
 		else {
-			if (this.owner.getDistanceSqToEntity(this.followingEntity) < 6.25D) // proximity to player
-				this.owner.getNavigator.clearPathEntity()
-			else {
+			if (!this.canFly) {
 				this.owner.getNavigator.tryMoveToEntityLiving(this.followingEntity, this.speed)
 			}
+			else {
+				val diffVect = new Vect(this.owner) - new Vect(this.followingEntity)
+				val diffNormalized = diffVect.norm
+				val diffScaled = diffNormalized * 0.3D * this.speed
+				this.owner.motionX = -diffScaled.x
+				this.owner.motionY = -diffScaled.y
+				this.owner.motionZ = -diffScaled.z
+			}
 		}
-
-
 
 	}
 
