6b55fb87052210e05b204b182dc4074099b2b33a
TheTemportalist <the.country.gamer@gmail.com>
2016-06-06 14:37:55 -0400

    Add test code for hostile abilities

--- src/main/scala/temportalist/esotericraft/emulation/common/ability/AbilityHostile.scala
+++ src/main/scala/temportalist/esotericraft/emulation/common/ability/AbilityHostile.scala
@@ -1,9 +1,13 @@
 package temportalist.esotericraft.emulation.common.ability
 
+import com.google.common.base.Predicate
+import net.minecraft.entity.monster.IMob
+import net.minecraft.entity.{Entity, EntityLiving, EntityLivingBase}
 import net.minecraft.nbt.NBTTagByte
-import temportalist.esotericraft.api.emulation.IAbility
 import temportalist.esotericraft.api.emulation.IAbility.Ability
 import temportalist.esotericraft.api.emulation.ability.IAbilityHostile
+
+import scala.collection.JavaConversions
 
 /**
   *
@@ -14,8 +18,52 @@ import temportalist.esotericraft.api.emulation.ability.IAbilityHostile
 @Ability(id = "hostile")
 class AbilityHostile extends AbilityBase[NBTTagByte] with IAbilityHostile {
 
+	private val radius = 10
+
 	// ~~~~~ Naming
 
 	override def getName: String = "Hostile"
+
+	// ~~~~~ Entity Handling
+
+	override def onUpdate(entity: EntityLivingBase): Unit = {
+
+		return // the below code does not work
+
+		// very similar to fear
+		// will make all mobs ignore you
+
+		val entityList = JavaConversions.asScalaBuffer(
+			entity.getEntityWorld.getEntitiesInAABBexcluding(entity,
+				entity.getEntityBoundingBox.expand(50, 50, 50),
+				new Predicate[Entity] {
+					override def apply(input: Entity): Boolean = {
+						input.isInstanceOf[IMob]
+					}
+				}
+			)
+		)
+
+		if (entityList.isEmpty) return
+
+		for (entityAround <- entityList) {
+			entityAround match {
+				case creature: EntityLiving =>
+
+					if (creature.getAttackTarget != null &&
+							creature.getAttackTarget.getEntityId == entity.getEntityId) {
+						creature.setAttackTarget(null)
+					}
+					if (creature.getAITarget != null &&
+							creature.getAITarget.getEntityId == entity.getEntityId) {
+						creature.setRevengeTarget(null)
+					}
+
+
+				case _ =>
+			}
+		}
+
+	}
 
 }
